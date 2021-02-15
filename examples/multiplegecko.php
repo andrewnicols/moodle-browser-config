481a82867eea82b0bd43995aae73a4618f2c26a0
Andrew Nicols <andrew@nicols.co.uk>
2021-02-15 09:39:55 +0800

    Provide example for parallel geckodriver

---
+++ examples/multiplegecko.php
@@ -0,0 +1,13 @@
+<?php
+
+use AndrewNicols\Behat\ProfileManager;
+
+$profiles = [];
+
+for ($i = 1; $i <= 3; $i++) {
+    // This is a hacky way to copy a profile.
+    // We'll add a better way in future.
+    $profile = array_merge([], $CFG->behat_profiles['headlessgeckodriver']);
+    $profile['wd_host'] = str_replace('4444', 4444 + $i - 1, $profile['wd_host']);
+    $profiles["headlessgeckodriver{$i}"] = $profile;
+}
